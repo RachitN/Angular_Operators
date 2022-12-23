443104bca02a9b32a2915de09d4512e34d568515
Rachit Nagwani <rachit.nagwani@decimal.co.in>
2022-12-23 20:25:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { throws } from 'assert';
+import { debounceTime, distinctUntilChanged, Observable, switchMap, tap, filter } from 'rxjs';
+import { Product } from './product';
+import {HttpClient} from '@angular/common/http'
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +12,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  fg :any = new FormGroup({
+    search : new FormControl(''),
+    result: new FormControl('')
+  })
+
   title = 'operator';
+  products: any;
+  baseUrl = environment.baseUrl
+
+  constructor(private http: HttpClient){
+    this.fg.controls.search.valueChanges.pipe(
+      debounceTime(400),
+      distinctUntilChanged(),
+      tap(()=>this.products =[]),
+      filter((filter:any) => filter.length>1),
+      
+      switchMap((term) => this.getProductsByName(term))
+    ).subscribe((resp:any) =>{
+      this.products = resp
+      this.fg.controls.result.setValue(this.products)
+    })
+  }
+
+  getProductsByName(name:any) : Observable<Product[]> {
+    return this.http.get<Product[]>(`${this.baseUrl}\products?name_like=${name}`);
+  } 
 }
---
+++ src/app/product.ts
@@ -0,0 +1,7 @@
+export class Product {
+    id: number | undefined;
+    name: string | undefined;
+    price: number | undefined;
+    image: string | undefined;
+    departament: string | undefined;
+}
